6d02621e416f647666ec50a4a34497879edb565e
Lin <lynnlae22@gmail.com>
2026-02-02 21:51:02 +0530

    Add GIF
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    ~
    "~/Desktop/BeMyValentine/.git/COMMIT_EDITMSG" 16L, 442BAdd GIF

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const noButton = document.getElementById("no");
 const yesButton = document.getElementById("yes");
 
@@ -32,3 +33,39 @@ yesButton.addEventListener("click", () => {
   document.body.innerHTML = "<h1>YAYYY! 🥹🥰</h1>";
   setInterval(createConfetti, 100);
 });
+=======
+const noButton = document.getElementById("no");
+const yesButton = document.getElementById("yes");
+
+noButton.addEventListener("mouseover", () => {
+  const x = Math.random() * 300;
+  const y = Math.random() * 300;
+
+  noButton.style.position = "absolute";
+  noButton.style.left = x + "px";
+  noButton.style.top = y + "px";
+});
+
+
+function createConfetti() {
+    const confetti = document.createElement("div");
+    confetti.classList.add("confetti");
+  
+    const colors = ["#ff4d6d", "#ffd166", "#06d6a0", "#118ab2", "#c77dff"];
+    confetti.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+  
+    confetti.style.left = Math.random() * window.innerWidth + "px";
+  
+    document.body.appendChild(confetti);
+  
+    setTimeout(() => {
+      confetti.remove();
+    }, 3000);
+  }  
+
+yesButton.addEventListener("click", () => {
+  document.body.innerHTML = "<h1>YAYYY! 🥹🥰</h1>";
+  setInterval(createConfetti, 100);
+});
+>>>>>>> 53223f9 (Add GIF)
